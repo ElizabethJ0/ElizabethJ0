85d910fd0aa9c61f888b77a37586738358243356
ElizabethJ0 <116313967+ElizabethJ0@users.noreply.github.com>
2023-01-17 23:01:27 +0000

    1/17

--- main.py
+++ main.py
@@ -2,19 +2,50 @@ import random
 #gameStart=False
 
 # if input="rock" 
-choice = input ("rock " " paper" " or scissors: ").lower()
-print("You choosed:", choice)
-list = ["rock" "paper" "scissors"]
+choose = input ("Choose one: "  "rock " " paper" " or scissors: ").lower()
+print("You choosed:", choose)
+list = [("rock"), ("paper"), ("scissors")]
+computer = random.choice(list)
+print("Computer choosed:", computer)
+
+# def rock(): 
+if choose == "rock" and computer == "scissors":
+  print ("You win")
+elif choose == "rock" and computer == "paper":
+  print ("You loose")
+elif choose == "rock" and computer == "rock":
+  print ("Try again")
+
+if choose == "scissors" and computer == "paper":
+  print("You win")
+elif choose == "scissors" and computer == "rock":
+  print ("You loose")
+elif choose == "scissors" and computer == "scissors":
+  print ("Try again")
+  
+  
+  
+
+# # if player picks rock, paper, or scissors the computer picks a different variable to play against to see who wins. 
+# Rock bets scissors
+# scissors bets paper
+# paper bets rock
+
+# if choice = rock 
 
 # if choice not in list:
   # print ("Not a valid answer. Choose rock, paper, or scissors")
 
 
 
-while choice not in list:
-  choice = input("Choose from the list").lower()
+# while choice not in list:
+  # choice = input("Choose from the list").lower()
+
+
+
   
-  # choice = input("Choose from list: " + str(options) + "").lower()
+  
+  # choice = input("Choose from list: " + str(choose) + "").lower()
    # if choice != ("rock" or "paper" or "scissors")
 
 
